8cf24a7b45d52493c7e47e78a35931e61d277bff
Trent Rivers <trentbrivers@gmail.com>
2025-11-11 19:17:36 -0600

    updated with database

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Trent Rivers in 3308!'
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://flaskgres_user:RDS8pFdV9v2mrARfaRZPYInr4tG1Cdj6@dpg-d49ttpbipnbc7399q8q0-a/flaskgres")
+
+    conn.close()
+    return "Database Connection Successful"
